0ee6e4c8302a79ea89e2d6962716629fa0fdc986
dimple-dhawan <dimple.dhawan@gmail.com>
2020-06-27 00:44:45 -0400

    Untrack files in .gitignore

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB081Z42baqkTJZEN2AqC_DwwxZveGvRa8';
-
-export const NO_AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB081Z42baqkTJZEN2AqC_DwwxZveGvRa8';
